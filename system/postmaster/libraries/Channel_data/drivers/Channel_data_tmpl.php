54a2a20aff71393a07a482a33e7eac3b0bda5c15
Justin Kimbrell <justin@objectivehtml.com>
2012-10-26 11:22:07 -0400

    Updated Channel Data

--- system/postmaster/libraries/Channel_data/drivers/Channel_data_tmpl.php
+++ system/postmaster/libraries/Channel_data/drivers/Channel_data_tmpl.php
@@ -12,7 +12,7 @@
  * @copyright	Copyright (c) 2012, Justin Kimbrell
  * @link 		http://www.objectivehtml.com/libraries/channel_data
  * @version		0.8.9
- * @build		20121025
+ * @build		20121026
  */
  
 class Channel_data_tmpl extends Channel_data_lib {
