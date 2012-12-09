700c085a7212cb54b5dd7eb771e4c33bab878979
Justin Kimbrell <justin@objectivehtml.com>
2012-12-09 11:13:10 -0500

    Update

---
+++ system/postmaster/hooks/Member_member_logout.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Member_member_logout_postmaster_hook extends Base_hook {
+	
+	protected $title = 'Member Logout';
+	
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
