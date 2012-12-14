f5b9592c57322a93a23b011c5198092950a7313f
Justin Kimbrell <justin@objectivehtml.com>
2012-12-14 08:40:27 -0500

    Added Profile:Edit hooks for new/updating profile

---
+++ system/postmaster/hooks/Profile_edit_end.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Profile_edit_end_postmaster_hook extends Base_hook {
+	
+	protected $title = 'Profile Edit End';
+	
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function trigger($member_id, $member_data, $entry_data, $success, $is_admin)
+	{
+		if($success)
+		{
+			$entry = $this->channel_data->get_channel_entry($entry_data['entry_id'])->row_array();
+			
+			$password = isset($member_data['password']) ? $member_data['password'] : NULL;
+			
+			$vars = array_merge($entry_data, array(
+				'entry_id'  => $entry_data['entry_id'],
+				'member_id' => $member_id,
+				'is_admin'  => $is_admin,
+				'password'  => $password
+			));
+			
+			parent::send($vars, FALSE, $entry);
+		}	
+	}
+}
