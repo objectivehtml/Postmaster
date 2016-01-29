b76403bbaff35d64c64df13a69ecd628dc4db5b4
Justin Kimbrell <justin@objectivehtml.com>
2016-01-29 11:57:13 -0500

    Incremented version number

--- system/postmaster/config/postmaster_config.php
+++ system/postmaster/config/postmaster_config.php
@@ -4,7 +4,7 @@
  *	Version
 /* -------------------------------------- */
 
-$config['postmaster_version'] = '1.6.0';
+$config['postmaster_version'] = '1.6.1';
 
 if(!defined('POSTMASTER_VERSION'))
 {	
